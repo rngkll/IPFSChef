7719c6ee7e2e05d0b5c5dc3dc83e3a145410bd91
Alvaro Segura Del Barco <alvarosb@gmail.com>
2018-04-15 13:03:45 -0600

    script to use ffmpeg to create dash files

--- mpeg-dash-stream.sh
+++ mpeg-dash-stream.sh
@@ -8,6 +8,9 @@ then
 	curl https://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_stereo.ogg -o big_buck_bunny_1080p_h264.ogg
 fi
 
+OUTPUT="bbb"
+DDIR="stream"
+
 $FFMPEG -re -i big_buck_bunny_1080p_stereo.ogg \
 	-map 0:0 \
 	-pix_fmt yuv420p \
@@ -16,11 +19,11 @@ $FFMPEG -re -i big_buck_bunny_1080p_stereo.ogg \
 	-r 24 -g 96 -keyint_min 96 -sc_threshold 0 \
 	-framerate 24\
 	-f segment \
-	-segment_format mp4 "${OUTPUT}_400k_%d.dash" \
+	-segment_format mp4 "${DDIR}/${OUTPUT}_400k_%d.dash" \
 	-map 0:1 \
 	-codec:a aac \
 	-b:a 128k \
 	-ar 48000 \
 	-f segment \
-	-segment_format mp4 "${OUTPUT}_128k_%d.dash"
+	-segment_format mp4 "${DDIR}/${OUTPUT}_128k_%d.dash"
 
